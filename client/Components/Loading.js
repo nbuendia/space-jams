84df6479805530f1b1c34d0399f695bb8b403838
nbuendia <nicole.buendia424@gmail.com>
2022-02-07 21:46:23 -0500

    loading compnent that should show up before displaying page, will have another loader for nav bar links

---
+++ client/Components/Loading.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+//MUI
+import { Box, Typography } from "@mui/material";
+
+function Loading() {
+  return (
+    <Box className="loading-container">
+        <Typography fontSize={30} fontWeight={800} color={"whitesmoke"}>
+            Waiting For The Sun to Eluminate The Earth...
+        </Typography>
+        <br />
+        <img
+            src="https://media.giphy.com/media/3o7WIB00yXujVt4WEo/giphy-downsized-large.gif"
+            width="480"
+            height="480"
+            frameBorder="0"
+            allowFullScreen
+        />
+    </Box>
+  );
+}
+
+export default Loading;
