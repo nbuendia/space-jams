4870388952010aabbc29f71054f556be57c8c186
nbuendia <nicole.buendia424@gmail.com>
2022-02-07 21:41:35 -0500

    started layout for home component

--- client/Components/Home.js
+++ client/Components/Home.js
@@ -1,8 +1,21 @@
 import React from "react";
 
+//MUI
+import { Box, Grid, Typography } from "@mui/material";
+
 function Home() {
     return(
-        <h1>HELLO WORLD</h1>
+        <>
+            <Grid item>
+                <Box style={{width: '85vw', display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <Typography>
+                        <b>HOME HOME HOME</b>
+                    </Typography>
+                    <br/>
+                    <img src="https://i.imgur.com/wqPqOII.jpg"/>
+                </Box>
+            </Grid>
+        </>
     )
 
 }
