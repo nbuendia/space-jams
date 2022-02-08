c9103312e370e38b9cadc23bab0743162c636347
nbuendia <nicole.buendia424@gmail.com>
2022-02-07 21:40:34 -0500

    added theme to entire project to have specific font

--- client/index.js
+++ client/index.js
@@ -2,14 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
+//MUI
+import { createTheme, ThemeProvider } from "@mui/material";
+
 //TO IMPORT STYLES ON DEPLOYED BUILD
 import '../public/style.css';
 
 //COMPONENTS
 import App from './App'
 
+//SET TYPOGRAPHY FONT TO 'Shadows Into Light'
+const theme = createTheme({
+    typography: {
+        fontFamily: 'Shadows Into Light'
+    }
+});
+
 ReactDOM.render(
     <Router>
-        <App />
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
     </Router>, document.getElementById('root')
 );
