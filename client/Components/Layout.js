e77546a2bc4e4feac1867d77e16ffe43c660b8ba
nbuendia <nicole.buendia424@gmail.com>
2022-02-07 21:47:07 -0500

    main layout component, will add footer in the future

---
+++ client/Components/Layout.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//COMPONENTS
+import NavBar from "./NavBar";
+
+function Layout() {
+    return(
+        <>
+            <NavBar />
+        </>
+    )
+
+}
+
+export default Layout;
