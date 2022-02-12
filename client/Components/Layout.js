35c664f4b402a648566a868eb58eaa0ac243dfab
nbuendia <nicole.buendia424@gmail.com>
2022-02-11 19:41:44 -0500

    added layout fucntionality from nac bar to this component. wanted to sepreate functionality

--- client/Components/Layout.js
+++ client/Components/Layout.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+
+//MUI
+import { Box } from "@mui/material";
 
 //COMPONENTS
 import NavBar from "./NavBar";
+import Home from "./landing-page/Home";
+import Epic from "./EPIC/Epic";
+import MarsRover from "./mars-rover/MarsRover";
+import Roadster from "./Roadster/Roadster";
+import ISS from "./international-space-station/ISS";
 
 function Layout() {
-    return(
-        <>
-            <NavBar />
-        </>
-    )
+    const [value, setValue] = useState('home');
 
+  return (
+    <Box className="nav-bar-container">
+        <NavBar value={value} setValue={setValue} />
+
+        <Box className="main-container">
+            {value === 'home' ? <Home />
+            : value === 'epic' ? <Epic />
+            : value === 'mars rover' ? <MarsRover />
+            : value === 'international space station' ? <ISS />
+            : <Roadster />}
+        </Box>
+    </Box>
+  );
 }
 
 export default Layout;
