41ea20d34b24f7448ae4d0a13772c29f02912f65
nbuendia <nicole.buendia424@gmail.com>
2022-02-12 19:15:06 -0500

    started most of home component

--- client/Components/landing-page/Home.js
+++ client/Components/landing-page/Home.js
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useStateIfMounted } from "use-state-if-mounted";
+import { RingLoader } from "react-spinners";
+import axios from "axios";
+
+//COMPONENTS
+import APOD from "./APOD";
 
 //MUI
-import { Box, Container, Grid, Typography } from "@mui/material";
+import { Box, Container } from "@mui/material";
+
+//DATA
+import key from '../../../secrets.json';
+const apiKey = key["api-key"];
+const url = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`;
 
 function Home() {
+    const [loading, isLoading] = useStateIfMounted(true);
+    const [data, getData] = useState(null);
+
+    useEffect(() => {
+        axios.get(url).then((res) => {
+            getData(res.data);
+            
+            //ADDED SO THE LOADING DOESNT LOOK LIKE A GLITCH, AXIOS REQ IS TOO FAST
+            setTimeout(() => {
+                isLoading(false);
+            }, 3000);
+
+        }).catch((err) => {
+            console.error('Oh No! Something went wrong!', err);
+        });
+
+    }, []);
+
+    //DISPLAYS LOADING WHILE AXIOS IS FETCHING IMAGES
+    if (loading) return (
+        <Box className="loader">
+            <RingLoader loading={isLoading} color="white" size={100} />
+        </Box>
+    );
+
     return(
         <Container>
-            <Grid item>
-                <Box className="inner-main-container">
-                    <Typography>
-                        <b>HOME HOME HOME</b>
-                    </Typography>
-                    <br/>
-                    <img src="https://i.imgur.com/wqPqOII.jpg"/>
-                </Box>
-            </Grid>
+            <APOD data={data} />
         </Container>
     )
 
