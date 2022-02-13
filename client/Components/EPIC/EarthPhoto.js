ce0c77de1d7f6dd9077bdeee1693b33b64b8d2ea
nbuendia <nicole.buendia424@gmail.com>
2022-02-12 20:29:46 -0500

    earth photo component

---
+++ client/Components/EPIC/EarthPhoto.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+//MUI
+import { Fade, IconButton, Tooltip } from "@mui/material";
+//MUI ICONS
+import FullscreenIcon from "@mui/icons-material/Fullscreen";
+
+function EarthPhoto(props) {
+  const { elem } = props;
+
+  //split date from time and access only the date; split date at '-' and rejoin with '/'
+  let imgDate = elem.date.split(" ")[0].split("-").join("/");
+
+  return (
+    <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href={`https://epic.gsfc.nasa.gov/archive/natural/${imgDate}/png/epic_1b_${elem.identifier}.png`}
+    >
+        <Tooltip title="Fullsize Image" placement="top" TransitionComponent={Fade}>
+            <IconButton>
+                <FullscreenIcon />
+            </IconButton>
+        </Tooltip>
+    </a>
+  );
+}
+
+export default EarthPhoto;
