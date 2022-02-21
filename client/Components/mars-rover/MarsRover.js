439248c7d30a4e790db9850873ea419b02e419e2
nbuendia <nicole.buendia424@gmail.com>
2022-02-20 19:50:35 -0500

    added new array of urls to get data from api, added new axios call, seperated rovers with tabs like nav bar

--- client/Components/mars-rover/MarsRover.js
+++ client/Components/mars-rover/MarsRover.js
@@ -9,33 +9,56 @@ import Opportunity from "./Opportunity";
 import Spirit from "./Spirit";
 
 //MUI
-import { Box, Container } from "@mui/material";
+import { Box, Container, Tab, Tabs, Typography } from "@mui/material";
 
 //DATA
 import key from '../../../secrets.json';
 const apiKey = key["api-key"];
-const urls =[
+
+const manifestUrls =[
     `https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=${apiKey}`,
     `https://api.nasa.gov/mars-photos/api/v1/manifests/Opportunity/?api_key=${apiKey}`,
     `https://api.nasa.gov/mars-photos/api/v1/manifests/Spirit/?api_key=${apiKey}`
 ];
+const latestUrls =[
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/Curiosity/latest_photos?api_key=${apiKey}`,
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/latest_photos?api_key=${apiKey}`,
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/Spirit/latest_photos?api_key=${apiKey}`
+];
 
 function MarsRover() {
     const [isLoading, setLoading] = useStateIfMounted(true);
+    const [value, setValue] = useState('curiosity');
+
     const [curiosityData, getCuriosityData] = useState(null);
+    const [latestCuriosityData, getLatestCuriosityData] = useState(null);
+
     const [opportunityData, getOpportunityData] = useState(null);
+    const [latestOpportunityData, getLatestOpportunityData] = useState(null);
+    
     const [spiritData, getSpiritData] = useState(null);
+    const [latestSpiritData, getLatestSpiritData] = useState(null);
+
+    function handleChange(event, newValue) {
+        setValue(newValue);
+    }
 
     useEffect(() => {
-        axios.all(urls.map((url) => axios.get(url))).then((res) => {
+        axios.all(manifestUrls.map((url) => axios.get(url))).then((res) => {
             getCuriosityData(res[0].data.photo_manifest);
             getOpportunityData(res[1].data.photo_manifest);
             getSpiritData(res[2].data.photo_manifest);
-            
-            setTimeout(() => {
-                setLoading(false);
-            }, 3000);
 
+        }).then(() => {
+            axios.all(latestUrls.map((url) => axios.get(url))).then((res) => {
+                getLatestCuriosityData(res[0].data.latest_photos);
+                getLatestOpportunityData(res[1].data.latest_photos);
+                getLatestSpiritData(res[2].data.latest_photos);
+                                
+                setTimeout(() => {
+                    setLoading(false);
+                }, 3000);
+            })
         }).catch((err) => {
             console.error('Oh No! Something went wrong!', err);
         })
@@ -49,10 +72,30 @@ function MarsRover() {
     );
 
     return(
-        <Container style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center', textAlign: 'center'}}>
-            <Curiosity data={curiosityData} />
-            <Opportunity data={opportunityData} />
-            <Spirit data={spiritData} />
+        <Container>
+            <Box style={{backgroundColor: '#242424', marginBottom: '25px'}}>
+                <Tabs centered value={value} onChange={handleChange}>
+                    <Tab 
+                        label={<Typography fontSize={25} color={'whitesmoke'}>Curiosity</Typography>} 
+                        value='curiosity' 
+                        className='tab' 
+                    />
+                    <Tab 
+                        label={<Typography fontSize={25} color={'whitesmoke'}>Opportunity</Typography>} 
+                        value='opportunity' 
+                        className='tab' 
+                    />
+                    <Tab 
+                        label={<Typography fontSize={25} color={'whitesmoke'}>Spirit</Typography>} 
+                        value='spirit' 
+                        className='tab' 
+                    />
+                </Tabs>
+            </Box>
+
+            {value === 'curiosity' ? <Curiosity data={curiosityData} latest={latestCuriosityData} />
+            : value === 'opportunity' ? <Opportunity data={opportunityData} latest={latestOpportunityData} />
+            : <Spirit data={spiritData} latest={latestSpiritData} />}
         </Container>
     )
 
