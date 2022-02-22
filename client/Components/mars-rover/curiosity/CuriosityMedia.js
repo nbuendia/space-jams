7d1f376e462a1c7d184dd455a256a22df229d69c
nbuendia <nicole.buendia424@gmail.com>
2022-02-21 21:24:37 -0500

    changed mapping to specific camera based on radio button value, removed card content

--- client/Components/mars-rover/curiosity/CuriosityMedia.js
+++ client/Components/mars-rover/curiosity/CuriosityMedia.js
@@ -1,14 +1,23 @@
 import React from "react";
 
 //MUI
-import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { Card, CardMedia, Grid } from "@mui/material";
 
 function CuriosityMedia(props) {
-    const { media } = props;
+    const { value, FHAZ, RHAZ, MAST, CHEMCAM, MAHLI, MARDI, NAVCAM } = props;
+
+    let camValue = value === 'FHAZ' ? FHAZ
+                 : value === 'RHAZ' ? RHAZ
+                 : value === 'MAST' ? MAST
+                 : value === 'CHEMCAM' ? CHEMCAM
+                 : value === 'MAHLI' ? MAHLI
+                 : value === 'MARDI' ? MARDI
+                 : value === 'NAVCAM' ? NAVCAM
+                 : null;
 
     return(
         <>
-            {media.map((img) => (
+            {camValue !== null ? camValue.map((img) => (
                 <Grid item key={img.id} xs={12} sm={6} md={4}>
                     <Card elevation={0} style={{backgroundColor: 'transparent'}}>
                         <CardMedia 
@@ -16,16 +25,9 @@ function CuriosityMedia(props) {
                             image={img.img_src}
                             alt='Latest Curiosity Rover Photo'
                         />
-
-                        <CardContent>
-                            <Typography fontFamily={'Patrick Hand'} fontSize={15} textAlign={"center"} color={'whitesmoke'}>
-                                Rover Camera: <br/>
-                                {img.camera.full_name}
-                            </Typography>
-                        </CardContent>
                     </Card>
                 </Grid>
-            ))}
+            )) : ''}
         </>
     )
 
