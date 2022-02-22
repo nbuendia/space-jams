7645f4954069c98c1b664477b5f728769f6e8792
nbuendia <nicole.buendia424@gmail.com>
2022-02-21 21:22:35 -0500

    cleaned up date, deleted console logs, changed classname

--- client/Components/mars-rover/curiosity/Curiosity.js
+++ client/Components/mars-rover/curiosity/Curiosity.js
@@ -9,17 +9,15 @@ import { Box, Paper, Typography } from "@mui/material";
 function Curiosity(props) {
     const { data, latest } = props;
 
-    const launchDate = data.launch_date.split('-');
-    const landingDate = data.landing_date.split('-');
-    const formattedLaunchDate = new Date(launchDate[0], launchDate[1], launchDate[2]).toDateString();
-    const formattedLandingDate = new Date(landingDate[0], landingDate[1], landingDate[2]).toDateString();
-
-    console.log('DATA', data);
-    console.log('LATEST', latest);
+    //CHANGING DATE TO LOOK FROM YYYY-MM-DD TO ddd MMM DD YYYY
+    let launchDate = data.launch_date.split('-');
+    let landingDate = data.landing_date.split('-');
+    launchDate = new Date(launchDate[0], launchDate[1] - 1, launchDate[2]).toDateString();
+    landingDate = new Date(landingDate[0], landingDate[1] - 1, landingDate[2]).toDateString();
 
     return(
         <>
-            <Box className="prev-apod-card" style={{marginBottom: '25px'}}>
+            <Box className="curiosity-info">
                 <Typography fontFamily={'Shizuru'} fontSize={25} textAlign={'center'}>
                     Rover Info: 
                 </Typography>
@@ -28,8 +26,8 @@ function Curiosity(props) {
                 <Paper elevation={5} style={{backgroundColor: 'transparent', padding: '10px'}}>
                     <Typography fontFamily={'Patrick Hand'} textAlign={'center'} color={'white'}>
                         Status: {data.status[0].toUpperCase() + data.status.slice(1)} <br/>
-                        Launch Date: {formattedLaunchDate} <br/>
-                        Landing Date: {formattedLandingDate}
+                        Launch Date: {launchDate} <br/>
+                        Landing Date: {landingDate}
                     </Typography>
                 </Paper>
             </Box>
