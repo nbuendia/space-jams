dc849d8f9aae306e6952db417855b7d55f0cdf20
nbuendia <nicole.buendia424@gmail.com>
2022-02-26 15:20:24 -0500

    used date prop instead of extracting date from first date in latest array

--- client/Components/mars-rover/curiosity/CuriosityLatest.js
+++ client/Components/mars-rover/curiosity/CuriosityLatest.js
@@ -4,30 +4,31 @@ import React from "react";
 import CuriosityRadioAndMedia from "./CuriosityRadioAndMedia";
 
 //MUI
-import { Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 function CuriosityLatest(props) {
-    const { latest } = props;
+    const { latest, cameras, date, sol } = props;
 
     //CHANGING DATE TO LOOK FROM YYYY-MM-DD TO ddd MMM DD YYYY
-    let date = latest[0].earth_date.split('-');
-    date = new Date(date[0], date[1] - 1, date[2]).toDateString();
-    const sol = latest[0].sol;
+    let formattedDate = date.split('-');
+    formattedDate = new Date(formattedDate[0], formattedDate[1] - 1, formattedDate[2]).toDateString();
     
     return(
         <>
             <hr/>
-            <Typography fontFamily={'Shizuru'} fontSize={25} textAlign={'center'}>
+            <Typography fontFamily={'Shizuru'} fontSize={30} textAlign={'center'}>
                 Most Recent Curiosity Rover Photos
             </Typography>
             <hr/>
-            
-            <Typography fontFamily={'Patrick Hand'} textAlign={"center"} color={'whitesmoke'} margin={'15px'}>
-                Taken on: {date} <br/>
-                Sol Day: {sol}
-            </Typography>
 
-            <CuriosityRadioAndMedia latest={latest} />
+            <Box className="curiosity-info">
+                <Typography fontFamily={'Patrick Hand'} fontSize={20} textAlign={"center"} color={'whitesmoke'} margin={'15px'} border={'1px solid #0366fc'}>
+                    Taken on: {formattedDate} <br/>
+                    Sol Day: {sol}
+                </Typography>
+            </Box>
+
+            <CuriosityRadioAndMedia latest={latest} cameras={cameras}/>
         </>
     )
 
